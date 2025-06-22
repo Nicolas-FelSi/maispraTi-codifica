905f70a4628148f82a59c7bee4d48b42369f7d28
Nicolas-FelSi <nicolasfelipe3702@gmail.com>
2025-06-22 14:17:35 -0300

    organizando repositório e iniciando 2 lista de exercicios

---
+++ modulo1/ListaExercicios2/ex1.js
@@ -0,0 +1,14 @@
+// 1. Validação de Datas
+// Crie a função ehDataValida(dia, mes, ano) que retorne true se os valores
+// formarem uma data real (meses de 28–31 dias, ano bissexto para
+// fevereiro) e false caso contrário.
+
+function ehDataValida(dia, mes, ano) {
+    
+    if (condition) {
+        return true;
+    } else {
+        return false;
+    }
+    
+}
